f7f918e6d8daeb8157481af275a888fcef91dfb3
cano_ing <josebulac960@gmail.com>
2023-08-06 21:20:40 -0500

    v.0.0.1.alpa

---
+++ contra.js
@@ -0,0 +1,18 @@
+function ir(){
+  var c = 1234;
+  var u = "xd";
+  if(document.form.password.value == c && document.form.login.value == u){
+  alert("tan bien");
+  window.location="periodico.html"
+  alert
+  
+
+}else {
+    alert("xd error xd")
+}
+
+
+    
+    
+}
+
