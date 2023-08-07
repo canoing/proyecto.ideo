da43523563fc0bb5acce276a1231d5dc89d9498d
cano_ing <josebulac960@gmail.com>
2023-08-07 16:08:19 -0500

    2

--- contra.js
+++ contra.js
@@ -2,7 +2,7 @@ function ir(){
   var c = 1234;
   var u = "xd";
   if(document.form.password.value == c && document.form.login.value == u){
-  alert("tan bien");
+ 
   window.location="periodico.html"
   alert
   
